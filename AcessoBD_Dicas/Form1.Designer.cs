d4f05c2023507a780950509863ae30027c6fdc5c
Silvair L. Soares <silvairsoares@outlook.com>
2020-03-30 16:35:44 -0300

    Add files via upload

---
+++ AcessoBD_Dicas/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace AcessoBD_Dicas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.dgvDados = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInserir.Location = new System.Drawing.Point(21, 235);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(146, 49);
+            this.btnInserir.TabIndex = 0;
+            this.btnInserir.Text = "Inserir Dados";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAtualizar.Location = new System.Drawing.Point(179, 235);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(146, 49);
+            this.btnAtualizar.TabIndex = 0;
+            this.btnAtualizar.Text = "Atualizar Dados";
+            this.btnAtualizar.UseVisualStyleBackColor = true;
+            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluir.Location = new System.Drawing.Point(340, 235);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(146, 49);
+            this.btnExcluir.TabIndex = 0;
+            this.btnExcluir.Text = "Excluir Dados";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // dgvDados
+            // 
+            this.dgvDados.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.dgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDados.Location = new System.Drawing.Point(21, 12);
+            this.dgvDados.Name = "dgvDados";
+            this.dgvDados.Size = new System.Drawing.Size(465, 209);
+            this.dgvDados.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(513, 306);
+            this.Controls.Add(this.dgvDados);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnAtualizar);
+            this.Controls.Add(this.btnInserir);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Acesso ao MS Access com C#";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button btnAtualizar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.DataGridView dgvDados;
+    }
+}
+
---
+++ AcessoBD_Dicas/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace AcessoBD_Dicas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ExibirDados();
+        }
+
+        private void ExibirDados()
+        {
+            // definir a string de conexão
+            string sDBstr = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=dados\Teste.mdb";
+
+            //definir a string SQL
+            string sSQL = "SELECT * from Clientes";
+
+            //criar o objeto connection
+            OleDbConnection oCn = new OleDbConnection(sDBstr);
+            //abrir a conexão
+            oCn.Open();
+            //criar o data adapter e executar a consulta 
+            OleDbDataAdapter oDA = new OleDbDataAdapter(sSQL, oCn);
+            //criar o DataSet
+            DataSet oDs = new DataSet();
+            //Preencher o dataset coom o data adapter
+            oDA.Fill(oDs, "Clientes");
+
+            //exibe os dados no datagridview
+            //exibe o resultado
+            dgvDados.DataSource = oDs.Tables[0];
+
+            // liberar o data adapter , o dataset e a conexao
+            oDA.Dispose();
+            oDs.Dispose();
+            oCn.Dispose();
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            // definir a string de conexão
+            string sDBstr = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=dados\Teste.mdb";
+
+            //definir a string SQL
+            string sSQL = "SELECT * from Clientes";
+
+            //criar o objeto connection
+            OleDbConnection oCn = new OleDbConnection(sDBstr);
+            //abrir a conexão
+            oCn.Open();
+            //criar o data adapter e executar a consulta 
+            OleDbDataAdapter oDA = new OleDbDataAdapter(sSQL, oCn);
+            //criar o DataSet
+            DataSet oDs = new DataSet();
+            //Preencher o dataset coom o data adapter
+            oDA.Fill(oDs, "Clientes");
+
+            //criar um objeto Data Row
+            DataRow oDR = oDs.Tables["Clientes"].NewRow();
+
+            //Preencher o datarow com valores
+            oDR["Nome"] = "Testolino";
+            oDR["Email"] = "teste@teste.com.br";
+         
+            //Incluir um datarow ao dataset
+            oDs.Tables["Clientes"].Rows.Add(oDR);
+            //Usar o objeto Command Bulder para gerar o Comandop Insert 
+            OleDbCommandBuilder oCB = new OleDbCommandBuilder(oDA);
+            //Atualizar o BD com valores do Dataset 
+            oDA.Update(oDs, "Clientes");
+
+            //liberar o data adapter , o dataset , o comandbuilder e a conexao
+            oDA.Dispose();
+            oDs.Dispose();
+            oCB.Dispose();
+            oCn.Dispose();
+
+            //exibir o resultado
+            ExibirDados();
+        }
+
+       
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            // definir a string de conexão
+            string sDBstr = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=dados\Teste.mdb";
+
+            //definir a string SQL
+            string sSQL = "SELECT * from Clientes";
+
+            //criar o objeto connection
+            OleDbConnection oCn = new OleDbConnection(sDBstr);
+            //abrir a conexão
+            oCn.Open();
+            //criar o data adapter e executar a consulta 
+            OleDbDataAdapter oDA = new OleDbDataAdapter(sSQL, oCn);
+            //criar o DataSet
+            DataSet oDs = new DataSet();
+            //Preencher o dataset coom o data adapter
+            oDA.Fill(oDs, "Clientes");
+            //cria o DataSet atribuindo ao DataRow o valor da linha que desejamos atualizar
+            DataRow oDR = oDs.Tables["Clientes"].Rows[5];
+
+            //Preenche o datarow with valores
+            oDR["Nome"] = "Maria";
+            oDR["Email"] = "maria@net.com.br";
+            
+            //Usar o objeto Command Bulder para gerar o Comando Update
+            OleDbCommandBuilder oCB = new OleDbCommandBuilder(oDA);
+            //Atualizar o BD com valores do Dataset 
+            oDA.Update(oDs, "Clientes");
+
+            //liberar o data adapter , o dataset , o comandbuilder e a conexao
+            oDA.Dispose();
+            oDs.Dispose();
+            oCB.Dispose();
+            oCn.Dispose();
+
+            //exibir o resultado
+            ExibirDados();
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            // definir a string de conexão
+            string sDBstr = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=dados\Teste.mdb";
+
+            //definir a string SQL
+            string sSQL = "SELECT * from Clientes";
+
+            //criar o objeto connection
+            OleDbConnection oCn = new OleDbConnection(sDBstr);
+            //abrir a conexão
+            oCn.Open();
+            //criar o data adapter e executar a consulta 
+            OleDbDataAdapter oDA = new OleDbDataAdapter(sSQL, oCn);
+            //criar o DataSet
+            DataSet oDs = new DataSet();
+            //Preencher o dataset coom o data adapter
+            oDA.Fill(oDs, "Clientes");
+            //Exclui a linha desejada
+            oDs.Tables["Clientes"].Rows[5].Delete();
+
+            // Usar o objeto Command Bulder para gerar o Comandop Delete
+            OleDbCommandBuilder oCB = new OleDbCommandBuilder(oDA);
+            //Atualizar o BD com valores do Dataset 
+            oDA.Update(oDs, "Clientes");
+
+            //liberar o data adapter , o dataset , o comandbuilder e a conexao
+            oDA.Dispose();
+            oDs.Dispose();
+            oCB.Dispose();
+            oCn.Dispose();
+
+            //exibir o resultado
+            ExibirDados();
+        }
+    }
+}
